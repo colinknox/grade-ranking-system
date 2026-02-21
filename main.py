e7993f1dba3eaa00c05dcddde183ec05cf70d61c
colinknox <colinwknox@gmail.com>
2026-02-21 17:26:00 -0500

    created get_numeric_value method

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ class Grade:
         self.letter = letter
         self.grade_index = GRADES.index(self.letter)
 
+    def get_numeric_value(self):
+        return self.grade_index
+
 
 
 a = Grade("A")
+
+print(f"DEBUG: Numeric value = {a.get_numeric_value()}")
