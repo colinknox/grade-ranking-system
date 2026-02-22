90c467350fd45473a4f6d9c24fb3a0a36106ef74
colinknox <colinwknox@gmail.com>
2026-02-22 09:20:36 -0500

    completed is_better_than method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-GRADES = ["B", "B", "D", "A", "C"]
+GRADES = ["F", "D", "C", "B", "A"]
 
 class Grade:
     def __init__(self, letter):
@@ -7,9 +7,17 @@ class Grade:
 
     def get_numeric_value(self):
         return self.grade_index
+    
+    def is_better_than(self, other):
+        if self.grade_index > other.grade_index:
+            return True
+        else:
+            return False
 
 
 
+b = Grade("B")
 a = Grade("A")
 
-print(f"DEBUG: Numeric value = {a.get_numeric_value()}")
+# print(f"DEBUG: Numeric value = {a.get_numeric_value()}")
+print(f"DEBUG: Is better than = {b.is_better_than(a)}")
