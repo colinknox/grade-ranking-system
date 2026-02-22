11395ade057818be512a7a4c437fdd01552dd591
colinknox <colinwknox@gmail.com>
2026-02-22 11:03:35 -0500

    refactored code and removed test code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+GRADES = ["F", "D", "C", "B", "A"]
+
 class Grade:
     def __init__(self, letter):
         self.letter = letter
@@ -7,13 +9,7 @@ class Grade:
         return self.grade_index
     
     def is_better_than(self, other):
-        if self.grade_index > other.grade_index:
-            return True
-        else:
-            return False
+        return self.grade_index > other.grade_index
 
     def is_same_as(self, other):
-        if self.grade_index == other.grade_index:
-            return True
-        else:
-            return False
+        return self.grade_index == other.grade_index
